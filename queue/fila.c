a1f84c909d1b00d265ab22367929b4706011a231
Deivid Almeida <deividalmeida365@gmail.com>
2021-02-14 15:44:09 -0300

    working on queue

--- queue/fila.c
+++ queue/fila.c
@@ -2,24 +2,43 @@
 #include <stdlib.h>
 #include <locale.h>
 
+
 #define N 3
 // filas estáticas //
 // segue o conceito de FIFO - First in - First out //
 
 
-typedef struct FILA {
+typedef struct  T_FILA {
 
     int dados[N];
     int fim;
 
-} FILA;
+} T_FILA;
 
-
-
-void iniciarFila(FILA * f ) {
+// iniciar o vetor em 0, passando um ponteiro para apontar para o typedef
+void inicializaFila(T_FILA * f) {
     int i;
 
-    for(i = 0; i < N; i++) {
+    for (i = 0; i < N; i++) {
         f->dados[i] = 0;
     }
+
+    f->fim = 0;
+    
+}
+
+
+void enfileira( int dado, T_FILA * f) {
+    if(f->fim == N) { // a fila está cheia e não pode inserir mais dados // 
+        printf("Fila está cheia!!");
+    } else {
+        f->dados[f->fim] = dado;
+        f->fim++;
+    }
+} 
+
+
+
+int main() {
+    
 }
